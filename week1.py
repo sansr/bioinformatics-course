fbfe3933fae05468305b65f2b267405a0b439dfb
Sandra Saez <san.saezr@gmail.com>
2016-05-01 17:37:58 +0200

    Added commens and chaged param names

--- week1.py
+++ week1.py
@@ -1,41 +1,47 @@
 import sys
 
-def PatternCount(pattern, text):
+def PatternCount(Pattern, Genome):
+    """Returns the number of times a pattern appears in Genome"""
     count = 0
-    for i in range(len(text)-len(pattern)+1):
-        if text[i:i+len(pattern)] == pattern:
+    for i in range(len(Genome)-len(Pattern)+1):
+        if Genome[i:i+len(Pattern)] == pattern:
             count += 1
     return count
 
-def CountDict(Text, k):
+def CountDict(Genome, k):
+    """Returns a dictionary in which associates the index of
+    each k-mer (pattern of length k) with the number of ocurrences"""
     Count = {}
-    for i in range(len(Text)-k+1):
-        Pattern = Text[i:i+k]
-        Count[i] = PatternCount(Pattern, Text)
+    for i in range(len(Genome)-k+1):
+        Pattern = Genome[i:i+k]
+        Count[i] = PatternCount(Pattern, Genome)
     return Count
 
-def FrequentWords(Text, k):
+def FrequentWords(Genome, k):
+    """Returns most frequent k-mer in Genome"""
     FrequentPatterns = []
-    Count = CountDict(Text, k)
+    Count = CountDict(Genome, k)
     m = max(Count.values())
     for i in Count:
         if Count[i] == m:
-            FrequentPatterns.append(Text[i:i+k])
+            FrequentPatterns.append(Genome[i:i+k])
     return list(set(FrequentPatterns))
 
-def ReverseComplement(Text):
+def ReverseComplement(Genome):
+    """Computes the Genome's reverse complement"""
     complements = {'A':'T', 'C':'G', 'G':'C', 'T':'A'}
-    rText = reversed(Text)
+    rText = reversed(Genome)
     rcomplement = ''
     for n in rText:
         rcomplement += complements[n]
     return rcomplement
 
-def PatternMatching(pattern, genome):
-    indices = set()
+def PatternMatching(Pattern, Genome):
+    """Calculates the positions where Pattern appears in Genome"""
+    indices = list()
     offset = 0
-    i = genome.find(pattern, offset)
+    i = Genome.find(Pattern, offset)
     while i >= 0:
-        indices.add(str(i))
-        i = genome.find(pattern, i+1)
+        indices.append(str(i))
+        i = Genome.find(Pattern, i+1)
     return indices
